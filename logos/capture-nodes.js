68965df428f66ff7521fca0cae1916a0e5bd382f
Francesc Busquets <francesc@gmail.com>
2021-03-16 17:13:43 +0100

    :bento: Add more school logos

--- logos/capture-nodes.js
+++ logos/capture-nodes.js
@@ -6,7 +6,9 @@ const path = require('path');
 const chalk = require('chalk');
 
 const centres = require('./logos-to-capture.json');
-const LOGO_TAG = /<img id\s*=\s*"logo_entity" src\s*=\s*"(.+)"\s*>/;
+const LOGO_CHECK_TAG = /<img [^>]* id\s*=\s*"logo_entity"[^>]*>/;
+const IMG_GET_URL = /src\s*=\s*"([^"]+)"/
+// const LOGO_TAG = /<img id\s*=\s*"logo_entity" src\s*=\s*"(.+)"\s*>/;
 const GENERIC_LOGO = /\/2014\/09\/logo-(escola|centre1)\.png$/;
 const RAW_LOGOS = path.join(__dirname, 'raw');
 
@@ -14,18 +16,20 @@ const fetchCentre = (id, url) => {
   return fetch(url)
     .then(response => response.text())
     .then(html => {
-      const match = html.match(LOGO_TAG);
-      if (!match || match.length < 2) {
-        console.log(chalk.yellow('WARNING: ') + `El centre ${id} (${url}) no té cap logo a la portada`);
-        return null;
+      const tagMatch = html.match(LOGO_CHECK_TAG);
+      if (tagMatch && tagMatch.length > 0) {
+        const urlMatch = tagMatch[0].match(IMG_GET_URL);
+        if (urlMatch && urlMatch.length > 1)
+          return urlMatch[1];
       }
-      return match[1];
+      console.log(chalk.yellow('WARNING: ') + `El centre ${id} (${url}) no té cap logo a la portada`);
+      return null;
     })
 }
 
 const downloadLogo = (id, url, dest) => {
   const urlLower = url.toLowerCase();
-  const ext = urlLower.substr(urlLower.lastIndexOf('.') + 1);
+  let ext = urlLower.substr(urlLower.lastIndexOf('.') + 1);
   if (ext === 'jpeg')
     ext = 'jpg';
   const destFile = path.join(dest, `${id}.${ext}`);
@@ -59,7 +63,7 @@ try {
           if (!logoUrl)
             return true;
           else if (GENERIC_LOGO.test(logoUrl)) {
-            console.log(chalk.yellow('WARNING: ') + `El centre ${centre.id} encara té el logo genèric!`);
+            console.log(chalk.yellow('WARNING: ') + `El centre ${centre.id} (${centre.url}) encara té el logo genèric!`);
             return true;
           }
           return downloadLogo(centre.id, logoUrl, RAW_LOGOS);
